2662fba9e712faf8fa7ff66aba6a27f45dff4e72
santhoshi-15 <vlssanthoshivajjiparthi@gmail.com>
2025-09-23 23:17:57 +0530

    changes

---
+++ lablogix-frontend/src/pages/RegisterFaculty.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function RegisterFaculty() {
+  return (
+    <div>
+      <h2>Register Faculty Page</h2>
+      <p>This is a placeholder page for now.</p>
+    </div>
+  );
+}
---
+++ lablogix-frontend/src/pages/RegisterStudent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function RegisterStudent() {
+  return (
+    <div>
+      <h2>Register Student  Page</h2>
+      <p>This is a placeholder page for now.</p>
+    </div>
+  );
+}
