9a649b3f1ac80041d787301e250b84b3f1e042df
santhoshi-15 <vlssanthoshivajjiparthi@gmail.com>
2025-09-25 12:59:36 +0530

    changes

--- lablogix-backend/routes/auth.js
+++ lablogix-backend/routes/auth.js
@@ -8,11 +8,12 @@ import Faculty from "../models/Faculty.js";
 
 const router = express.Router();
 
-// POST /api/auth/login
+// POST login
 router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-
   try {
+    let { email, password } = req.body;
+    email = email.toLowerCase();
+
     let user = await Admin.findOne({ email });
     let role = "admin";
 
@@ -26,18 +27,12 @@ router.post("/login", async (req, res) => {
       role = "student";
     }
 
-    if (!user) {
-      return res.status(401).json({ message: "User not found or not registered yet" });
-    }
+    if (!user) return res.status(401).json({ message: "Invalid credentials" });
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(401).json({ message: "Invalid credentials" });
 
-    const token = jwt.sign(
-      { id: user._id, role },
-      process.env.JWT_SECRET,
-      { expiresIn: "8h" }
-    );
+    const token = jwt.sign({ id: user._id, role }, process.env.JWT_SECRET, { expiresIn: "8h" });
 
     res.json({ token, role, user });
   } catch (err) {
